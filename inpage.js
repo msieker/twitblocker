d6e85cc866498444885e7cf1422e627661b482b4
Matt Sieker <msieker@biomedsys.com>
2016-03-04 12:14:54 -0600

    -Add featherlight for lightbox - target things better -Add license

--- inpage.js
+++ inpage.js
@@ -1,18 +1,34 @@
 $(function(){
-    
-    function blockWithReason()
+    function blockWithReason(tweet)
     {
-        
+        console.log("Blocking with a reason", tweet);
     }
     
-    $(".stream-container").on("click",".ProfileTweet-actionButton", function(){
-        var button = $(this);
+    function addMenuItem(root, tweet){
+        var button = $(".block-with-reason",root);
+        if(!button.length){
+            console.log("adding menu item to", root);        
+            var li = $("<li>").addClass("embed-link");
+            button = $("<button>").addClass("dropdown-link block-with-reason");
+            button.attr("role","menuitem");
+            button.text("Block With Reason");
+            li.append(button);
+
+            button.click(function(){blockWithReason(tweet)});        
+            root.append(li);
+        }
+    }
+    
+    function startHook(button){
         var tweet = button.closest(".tweet");
         console.log("tweet:",tweet);
         var tweetId =
         
         setTimeout(function(){
-            console.log(button.parent());
+            addMenuItem($(".dropdown-menu ul",button.parent()),tweet);
         }, 5);
-    });
+    }
+    
+    $("#timeline").on("click",".ProfileTweet-actionButton", function(){startHook($(this));});
+    $(".PermalinkOverlay").on("click",".ProfileTweet-actionButton", function(){startHook($(this));});
 });
